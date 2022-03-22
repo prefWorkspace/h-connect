315823e3c90cc5acb01ec963213f9278e22eac5a
Jang Byung Yeon <kisagge@naver.com>
2022-03-22 17:31:44 +0900

    nurse dashboard 폴더구조 변경 및 파일 추가

--- H-Connect/js/nurse/dashboard/renders/renderWardList.js
+++ H-Connect/js/nurse/dashboard/renders/renderWardList.js
@@ -1,14 +1,14 @@
 // jquery dom
-const $ward_selectBox_el = $(".nurse.dashboard .taget_select .select_ward");
+const $ward_selectBox_el = $('.nurse.dashboard .taget_select .select_ward');
 const $ward_selectBox_label_el = $(
-  ".nurse.dashboard .taget_select .select_ward .ward_label"
+  '.nurse.dashboard .taget_select .select_ward .ward_label'
 );
 const $ward_option_el = $(
-  ".nurse.dashboard .taget_select .select_ward .ward_option"
+  '.nurse.dashboard .taget_select .select_ward .ward_option'
 );
 
 const wardSelect = (item) => {
-  $ward_selectBox_el.removeClass("active");
+  $ward_selectBox_el.removeClass('active');
   $ward_selectBox_label_el.html(item.text());
 };
 
@@ -18,10 +18,10 @@ if (Array.isArray($ward_option_el)) {
   console.log(2);
 }
 
-$ward_selectBox_label_el.on("click", () => {
-  if ($ward_selectBox_el.hasClass("active")) {
-    $ward_selectBox_el.removeClass("active");
+$ward_selectBox_label_el.on('click', () => {
+  if ($ward_selectBox_el.hasClass('active')) {
+    $ward_selectBox_el.removeClass('active');
   } else {
-    $ward_selectBox_el.addClass("active");
+    $ward_selectBox_el.addClass('active');
   }
 });
