d66cb6b9388c34148c35d7eeae54e41106708811
jikor1st <jikor1st@naver.com>
2022-03-23 16:17:37 +0900

    모니터링 > 전체환자보기 , 병실별로 보기리스트 작업완료

--- H-Connect/js/nurse/monitoring/renders/insertMonitoringRoom.js
+++ H-Connect/js/nurse/monitoring/renders/insertMonitoringRoom.js
@@ -1,31 +1,35 @@
 import {
-  monitorBlock_have,
-  monitorBlock_none,
-  monitorRoomBlock,
+    monitorBlock_have,
+    monitorBlock_none,
+    monitorRoomBlock,
 } from '../templates/monitoringPatientTemplate.js';
 import {
-  getMonitoringSickBed,
-  getPatientMeasurementInfoList,
-  getMonitoringSickRoom,
+    getMonitoringSickBed,
+    getPatientMeasurementInfoList,
+    getMonitoringSickRoom,
 } from '../actions/getMonitoringAll.js';
 
 async function monitoring_sickRoomList_insert() {
-  // sickRoomCode
-  // wardCode
-  const { sickRoomList } = await getMonitoringSickRoom(); // 병실 리스트
-  const { sickBedList } = await getMonitoringSickBed(); // 병상 리스트
-  const { measurementInfoSimpleList: patientList } =
-    await getPatientMeasurementInfoList(); // 환자 측정 리스트
-  // console.log(sickRoomList);
-  // console.log(sickBedList);
-  // console.log(patientList);
+    // sickRoomCode
+    // wardCode
+    const { sickRoomList } = await getMonitoringSickRoom(); // 병실 리스트
+    const { sickBedList } = await getMonitoringSickBed(); // 병상 리스트
+    const { measurementInfoSimpleList: patientList } =
+        await getPatientMeasurementInfoList(); // 환자 측정 리스트
+    // console.log(sickRoomList);
+    // console.log(sickRoomList);
+    // console.log(patientList);
 
-  let _html = '';
-  for (let i = 0, _sickRoomLen = sickRoomList?.length; i < _sickRoomLen; i++) {
-    const sickRoomItem = sickRoomList[i];
-    _html += monitorRoomBlock({ sickRoomItem, patientList });
-  }
+    let _html = '';
+    for (
+        let i = 0, _sickRoomLen = sickRoomList?.length;
+        i < _sickRoomLen;
+        i++
+    ) {
+        const sickRoomItem = sickRoomList[i];
+        _html += monitorRoomBlock({ sickRoomItem, patientList });
+    }
 
-  $('.nurse.patient_vital .wrap_inner .patient_room_wrap').html(_html);
+    $('.nurse.patient_vital .wrap_inner .patient_room_wrap').html(_html);
 }
 monitoring_sickRoomList_insert();
