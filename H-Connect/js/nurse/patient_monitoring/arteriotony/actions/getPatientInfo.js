dac86f09ffa590b577340e81b4c0af67129d74bd
SangMin <sangmin4208@gmail.com>
2022-03-23 10:51:20 +0900

    혈압 수동 입력페이지 리팩토링

---
+++ H-Connect/js/nurse/patient_monitoring/arteriotony/actions/getPatientInfo.js
@@ -0,0 +1,27 @@
+import { serverController } from '../../../../utils/controller/serverController.js';
+
+import { commonRequest } from '../../../../utils/controller/commonRequest.js';
+import { history } from '../../../../utils/controller/historyController.js';
+
+export const getPatientInfo = async () => {
+    /* s: 환자 측정 상세 정보 */
+    const res = await serverController.ajaxAwaitController(
+        'API/Measurement/SelectMeasurementInfoDetail',
+        'POST',
+        JSON.stringify({
+            ...commonRequest(),
+            measurementCode: history.getParams('measurement_code'),
+        })
+    );
+    if (!res.measurementInfo) {
+        throw new Error('환자 정보를 가져오는데 실패했습니다.');
+    }
+    return {
+        ...res.measurementInfo,
+        gender: decodeGender(res.measurementInfo.gender),
+    };
+};
+
+const decodeGender = (number) => {
+    return number === 1 ? '남' : '여';
+};
