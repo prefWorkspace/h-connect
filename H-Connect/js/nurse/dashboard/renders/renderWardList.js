e2872dadc40ccfd74a203035941880613ac01230
Jang Byung Yeon <kisagge@naver.com>
2022-03-24 14:47:15 +0900

    feat: 간호사 대시보드 모니터링 대상 병동에 따른 병실들 출력

--- H-Connect/js/nurse/dashboard/renders/renderWardList.js
+++ H-Connect/js/nurse/dashboard/renders/renderWardList.js
@@ -15,6 +15,12 @@ const dummy_wardList = [
                 nickname: '102호실',
                 roomTotalSickBed: 4,
             },
+            {
+                sickRoomCode: 'SEERS_01_03',
+                sickRoom: '103호실',
+                nickname: '103호실',
+                roomTotalSickBed: 6,
+            },
         ],
     },
     {
@@ -38,14 +44,28 @@ const dummy_wardList = [
     {
         ward: '치과',
         wardCode: 'SEERS_TEETH',
+        sickRoomList: [
+            {
+                sickRoomCode: 'SEERS_03_01',
+                sickRoom: '301호실',
+                nickname: '301호실',
+                roomTotalSickBed: 4,
+            },
+            {
+                sickRoomCode: 'SEERS_03_02',
+                sickRoom: '302호실',
+                nickname: '302호실',
+                roomTotalSickBed: 5,
+            },
+        ],
     },
 ];
 
 const dummy_patients = [
     {
         patientCode: 'PATI_1',
-        name: '김한식',
-        age: 50,
+        name: '박진형',
+        age: 28,
         gender: 1,
         sickRoomCode: 'SEERS_01_01',
     },
@@ -58,50 +78,141 @@ const dummy_patients = [
     },
     {
         patientCode: 'PATI_3',
-        name: '이한녀',
+        name: '이현영',
         age: 40,
         gender: 2,
         sickRoomCode: 'SEERS_01_02',
     },
 ];
+
 import {
     parseWardList,
-    parseSickRoom,
+    parsePatientsBySickRoom,
 } from '../templates/dashboardWardListTmpl.js';
 
 const $target_select = $('.taget_select');
+let selected_ward = 'NONE';
 
-export const renderDashboard = async () => {
+const renderDashboard = async () => {
     try {
-        const wardList = dummy_wardList;
-        const wardList_el = parseWardList(wardList);
-        const sickRoomList = parseSickRoom(
-            wardList[0].sickRoomList,
+        // 병동 선택
+        const wardList = parseWardList(dummy_wardList);
+        const patientList = parsePatientsBySickRoom(
+            dummy_wardList,
             dummy_patients
         );
-        $target_select.html(wardList_el + sickRoomList);
+        $target_select.html(wardList + patientList);
+
+        // 이벤트 바인딩
     } catch (err) {
         console.log(err);
     }
 };
 
-// function _dashboard_wardSelectBox_select() {
+function _dashboard_wardSelectBox_select() {
+    const _$ward_selectBox_el = $(
+        '.nurse.dashboard .taget_select .select_ward'
+    );
+    const _$ward_selectBox_selected_el = $(
+        '.nurse.dashboard .taget_select .select_ward .ward_label'
+    );
+    const _$ward_option_list_el = $(
+        '.nurse.dashboard .taget_select .select_ward .ward_option .ward_list'
+    );
+
+    const _wardSelect = (item) => {
+        _$ward_selectBox_el.removeClass('active');
+        _$ward_selectBox_selected_el.html(item.text());
+    };
+
+    _$ward_option_list_el.each(function () {
+        $(this)
+            .off()
+            .on('click', () => {
+                _wardSelect($(this));
+                selected_ward = $(this).attr('name');
+                showSickRoom(selected_ward);
+            });
+    });
+
+    _$ward_selectBox_selected_el.on('click', () => {
+        if (_$ward_selectBox_el.hasClass('active')) {
+            _$ward_selectBox_el.removeClass('active');
+        } else {
+            _$ward_selectBox_el.addClass('active');
+        }
+    });
+}
+
+function showSickRoom(wardCode) {
+    $('.ward_block').css('display', 'none');
+    if (wardCode) $(`.ward_block.${wardCode}`).css('display', 'block');
+}
+
+function _dashboard_target_monitoring_init() {
+    renderDashboard();
+    _dashboard_wardSelectBox_select();
+    showSickRoom(null);
+}
+
+_dashboard_target_monitoring_init();
+// const renderDashboard = async () => {
+//     try {
+//         const wardList = dummy_wardList;
+//         const wardList_el = parseWardList(wardList);
+//         const sickRoomList = parseSickRoom(
+//             wardList[0].sickRoomList,
+//             dummy_patients
+//         );
+//         $target_select.html(wardList_el + sickRoomList);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+
+// const attachEvents = async () => {
+//     try {
+//         const _$ward_selectBox_el = $(
+//             '.nurse.dashboard .taget_select .select_ward'
+//         );
+//         const _$ward_selectBox_selected_el = $(
+//             '.nurse.dashboard .taget_select .select_ward .ward_label'
+//         );
+
+//         const _$ward_option_list_el = $(
+//             '.nurse.dashboard .taget_select .select_ward .ward_option .ward_list'
+//         );
+//         _dashboard_wardSelectBox_select(
+//             _$ward_selectBox_el,
+//             _$ward_selectBox_selected_el,
+//             _$ward_option_list_el
+//         );
+//         _dashboard_sickRoom_select();
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+// function _dashboard_wardSelectBox_select(
+//     selectBox_el,
+//     selectBox_select,
+//     option_list_el
+// ) {
 //     const _wardSelect = (item) => {
-//         $ward_selectBox_el.removeClass('active');
-//         $ward_selectBox_selected_el.html(item.text());
+//         selectBox_el.removeClass('active');
+//         selectBox_select.html(item.text());
 //     };
 
-//     $ward_option_list_el.each(function () {
+//     option_list_el.each(function () {
 //         $(this)
 //             .off()
 //             .on('click', () => _wardSelect($(this)));
 //     });
 
-//     $ward_selectBox_selected_el.on('click', () => {
-//         if ($ward_selectBox_el.hasClass('active')) {
-//             $ward_selectBox_el.removeClass('active');
+//     selectBox_select.on('click', () => {
+//         if (selectBox_el.hasClass('active')) {
+//             selectBox_el.removeClass('active');
 //         } else {
-//             $ward_selectBox_el.addClass('active');
+//             selectBox_el.addClass('active');
 //         }
 //     });
 // }
@@ -129,18 +240,23 @@ export const renderDashboard = async () => {
 //         });
 // }
 
-// function _dashboard_sickRoom_checkSelect() {}
-
-// function _dashboard_addPatients() {}
-
-// function _dashboard_render() {
-//     _dashboard_wardSelectBox_select();
-
-//     _dashboard_sickRoom_select();
-
-//     _dashboard_addPatients();
+// export function getPatientsChecked() {
+//     let patients = $('input[name=patient_no]:checked');
+//     console.log(patients);
 // }
+// // function _dashboard_sickRoom_checkSelect() {}
 
-// _dashboard_render();
+// // function _dashboard_addPatients() {}
 
-renderDashboard();
+// // function _dashboard_render() {
+// //     _dashboard_wardSelectBox_select();
+
+// //     _dashboard_sickRoom_select();
+
+// //     _dashboard_addPatients();
+// // }
+
+// // _dashboard_render();
+
+// renderDashboard();
+// attachEvents();
