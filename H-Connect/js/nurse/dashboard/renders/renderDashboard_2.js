a4e92d3abfc8c244c9466bd68d2058deb384ce53
kisagge <kisagge@github.com>
2022-03-31 12:08:20 +0900

    간호사 대시보드 > 모니터링 대상 병상 체크박스 작업 완료

--- H-Connect/js/nurse/dashboard/renders/renderDashboard_2.js
+++ H-Connect/js/nurse/dashboard/renders/renderDashboard_2.js
@@ -40,6 +40,8 @@ const DISPLAY_MAX_BED = 26;
 
 // API variable
 const displayList = await getDisplayList(DISPLAY_START, DISPLAY_END);
+const { wardList } = await selectWardList();
+const { sickRoomList } = await selectSickRoomList();
 const { sickBedList } = await selectSickBedList();
 
 //전역 변수들
@@ -59,7 +61,6 @@ console.log(bed_number_by_display);
 // Rendering Monitoring Target Ward Select Box
 const renderWardSelectBox = async () => {
     try {
-        const { wardList } = await selectWardList();
         const templateWardSelect = await parseWardListLeft(wardList);
         $target_select.html(templateWardSelect);
         await addEventToWardSelectBox();
@@ -70,9 +71,6 @@ const renderWardSelectBox = async () => {
 
 const renderSickBedListLeft = async () => {
     const $select_head = $('.select_head');
-
-    const { sickRoomList } = await selectSickRoomList();
-    const { sickBedList } = await selectSickBedList();
     const templateSickBedList = await parseSickBedListLeft(
         sickRoomList,
         sickBedList
@@ -123,14 +121,6 @@ async function addEventToWardSelectBox() {
         return ele.data('wardcode');
     };
 
-    $ward_selectbox_selected.on('click', () => {
-        if ($ward_selectbox.hasClass('active')) {
-            $ward_selectbox.removeClass('active');
-        } else {
-            $ward_selectbox.addClass('active');
-        }
-    });
-
     $ward_selectbox_optionlist.each(function () {
         $(this)
             .off()
@@ -138,13 +128,67 @@ async function addEventToWardSelectBox() {
                 const changeWard = wardSelect($(this));
                 if (changeWard != selectedWard) {
                     selectedWard = changeWard;
-                    //showSickRoom(selectedWard);
+                    // 병동에 따른 병실 출력
                     $('.ward_block').css('display', 'none');
                     if (selectedWard)
                         $(`.ward_block.${selectedWard}`).css(
                             'display',
                             'block'
                         );
+                    // 병동 체크박스 이벤트 부여
+                    addEventOnWardCheck(selectedWard);
+                    //병동 바꿀 시 병동 선택되어 있으면 선택 해제
+                    isWardChecked();
+
+                    //모니터랑 대상 병실 병상 체크 해제
+                    $(`input[name=sickBed_no]`).prop('checked', false);
+                    // 병동 변경시 펼쳐진 병실병상 리스트 접기
+                    $('.ward_count.on').trigger('click');
+                    //병실 체크박스 이벤트
+                    addEventOnSickRoomCheck(selectedWard);
+                    //병상 체크박스 이벤트부여
+                    addEventOnSickBed(selectedWard);
+                    // 병동 바꿀 시 체크된 병상들 체크 해제
+                    allSickBedsUnchecked();
+                    // 병동 바꿀 시 체크된 병실들 체크 해제
+                    allSickRoomUnchecked();
+                }
+            });
+    });
+
+    $ward_selectbox_selected.on('click', () => {
+        if ($ward_selectbox.hasClass('active')) {
+            $ward_selectbox.removeClass('active');
+        } else {
+            $ward_selectbox.addClass('active');
+        }
+    });
+}
+
+// Add Event To Left SickRoom Arrow
+async function addEventToSickRoomArrow() {
+    sickRoomList.forEach((sickRoom) => {
+        $(`.ward_count.${sickRoom.sickRoomCode}`)
+            .off()
+            .on('click', () => {
+                if (
+                    $(`.patient_info.${sickRoom.sickRoomCode}`).css(
+                        'display'
+                    ) == 'block'
+                ) {
+                    $(`.patient_info.${sickRoom.sickRoomCode}`).css(
+                        'display',
+                        'none'
+                    );
+                    $(`.ward_count.${sickRoom.sickRoomCode}.on`).removeClass(
+                        'on'
+                    );
+                } else {
+                    $(`.patient_info.${sickRoom.sickRoomCode}`).css(
+                        'display',
+                        'block'
+                    );
+                    $(`.ward_count.${sickRoom.sickRoomCode}`).addClass('on');
                 }
             });
     });
@@ -160,6 +204,160 @@ async function addEventToDisplayBtn() {
                 $(this).addClass('on');
                 selected_display = $(this).data('id');
                 renderDashboardScreen(selected_display, sickBedList);
+            });
+    });
+}
+
+async function addEventOnWardCheck(wardCode) {
+    const $ward_check = $('#ward_check');
+    $ward_check.off().on('click', () => {
+        //병실 체크
+        const sickRoomByWard = wardList
+            .filter((ward) => ward.wardCode === wardCode)[0]
+            .sickRoomList.map((sickRoom) => sickRoom.sickRoomCode);
+        sickRoomByWard.forEach((sickRoomCode) => {
+            if ($ward_check.prop('checked')) {
+                $(`#${sickRoomCode}`).prop('checked', true);
+            } else {
+                $(`#${sickRoomCode}`).prop('checked', false);
+            }
+        });
+        // 병상 체크
+        const checkSickbedsArray = sickBedList
+            .filter((sickBed) => sickBed.wardCode === wardCode)
+            .map((bed) => bed.sickBedCode);
+        checkSickbedsArray.forEach((sickBedCode) => {
+            if ($ward_check.prop('checked')) {
+                $(`#${sickBedCode}`).prop('checked', true);
+            } else {
+                $(`#${sickBedCode}`).prop('checked', false);
+            }
+        });
+    });
+}
+
+//병동 바꿀 시 병동 선택되어 있으면 선택 해제
+async function isWardChecked() {
+    const $ward_check = $('#ward_check');
+    $ward_check.prop('checked', false);
+}
+
+//병실 선택 전체 해제
+async function allSickRoomUnchecked() {
+    const $sickRoomCheck = $('input[name=room_no]');
+    $sickRoomCheck.prop('checked', false);
+}
+
+async function allSickBedsUnchecked() {
+    const $sickBedCheck = $('input[name=sickBed_no]');
+    $sickBedCheck.prop('checked', false);
+}
+
+//병실 체크박스 이벤트 부여
+async function addEventOnSickRoomCheck(wardCode) {
+    const sickRoomByWard = wardList
+        .filter((ward) => ward.wardCode === wardCode)[0]
+        .sickRoomList.map((sickRoom) => sickRoom.sickRoomCode);
+    //다 체크되면 병동선택 체크하는 함수
+    function checkWardByCheckRoom(roomLength) {
+        let checkedLen = 0;
+        sickRoomByWard.forEach((sickRoomCode) => {
+            if ($(`#${sickRoomCode}`).prop('checked')) checkedLen += 1;
+        });
+
+        if (roomLength === checkedLen) {
+            $('#ward_check').prop('checked', true);
+        } else {
+            $('#ward_check').prop('checked', false);
+        }
+    }
+    sickRoomByWard.forEach((sickRoomCode) => {
+        const $sickRoom_check = $(`#${sickRoomCode}`);
+
+        $sickRoom_check.off().on('click', () => {
+            //병실 체크박스 체크시 이벤트 부여
+            checkWardByCheckRoom(sickRoomByWard.length);
+            const checkSickBedsArray = sickBedList
+                .filter((sickBed) => sickBed.sickRoomCode === sickRoomCode)
+                .map((sickBed) => sickBed.sickBedCode);
+            checkSickBedsArray.forEach((sickBedCode) => {
+                if ($sickRoom_check.prop('checked')) {
+                    $(`#${sickBedCode}`).prop('checked', true);
+                } else {
+                    $(`#${sickBedCode}`).prop('checked', false);
+                }
+            });
+        });
+    });
+}
+
+//병상 체크박스 이벤트부여
+async function addEventOnSickBed(wardCode) {
+    const sickRoomByWard = wardList
+        .filter((ward) => ward.wardCode === wardCode)[0]
+        .sickRoomList.map((sickRoom) => sickRoom.sickRoomCode);
+    function checkWardByCheckRoom(roomLength) {
+        let checkedLen = 0;
+        sickRoomByWard.forEach((sickRoomCode) => {
+            if ($(`#${sickRoomCode}`).prop('checked')) checkedLen += 1;
+        });
+        if (roomLength === checkedLen) {
+            $('#ward_check').prop('checked', true);
+        } else {
+            $('#ward_check').prop('checked', false);
+        }
+    }
+    //병상에 따른 병실 체크박스 제어
+    let sickBedsBySickRoomMany = {};
+    sickRoomByWard.forEach((sickRoomCode) => {
+        if (!sickBedsBySickRoomMany[sickRoomCode]) {
+            sickBedsBySickRoomMany[sickRoomCode] = 0;
+        }
+    });
+
+    const checkSickBedsArray = sickBedList
+        .filter((sickBed) => sickBed.wardCode === wardCode)
+        .map((sickBed) => {
+            sickBedsBySickRoomMany[sickBed.sickRoomCode] += 1;
+            return {
+                sickRoomCode: sickBed.sickRoomCode,
+                sickBedCode: sickBed.sickBedCode,
+            };
+        });
+
+    checkSickBedsArray.forEach((sickBed) => {
+        $(`#${sickBed.sickBedCode}`)
+            .off()
+            .on('click', () => {
+                const sickRoomCodeofSickBed = checkSickBedsArray.filter(
+                    (bed) => bed.sickBedCode === sickBed.sickBedCode
+                )[0].sickRoomCode;
+                let checkSickBed = 0;
+                const sickBedsBySickRoom = checkSickBedsArray
+                    .filter((bed) => bed.sickRoomCode === sickRoomCodeofSickBed)
+                    .map((b) => b.sickBedCode);
+                sickBedsBySickRoom.forEach((bed) => {
+                    if ($(`#${bed}`).prop('checked')) checkSickBed += 1;
+                });
+
+                if (
+                    checkSickBed ===
+                    sickBedsBySickRoomMany[sickRoomCodeofSickBed]
+                ) {
+                    $(`#${sickRoomCodeofSickBed}`).prop('checked', true);
+                } else {
+                    $(`#${sickRoomCodeofSickBed}`).prop('checked', false);
+                }
+
+                let checkSickBedByWard = 0;
+                checkSickBedsArray.forEach((bed) => {
+                    if ($(`#${bed.sickBedCode}`).prop('checked'))
+                        checkSickBedByWard += 1;
+                });
+                if (!checkSickBedsArray.length === checkSickBedByWard) {
+                    $(`#ward_check`).prop('checked', false);
+                }
+                checkWardByCheckRoom(sickRoomByWard.length);
             });
     });
 }
@@ -237,6 +435,8 @@ async function addEventToMakeDisplayPop() {
 async function firstRender() {
     await renderWardSelectBox();
     await renderSickBedListLeft();
+
+    addEventToSickRoomArrow();
 
     addEventToDeleteBtn();
     addEventToMakeDisplayPop();
--- H-Connect/js/nurse/dashboard/templates/templateSickBedListLeft.js
+++ H-Connect/js/nurse/dashboard/templates/templateSickBedListLeft.js
@@ -1,9 +1,63 @@
 import { getPatientList } from '../actions/getPatientList.js?v=2022.03.25.12.01';
 
 export const parseSickBedListLeft = async (sickRoomList, sickBedList) => {
+    function findPatientBySickBedCode(_sickBedCode, _patientList) {
+        for (let i = 0; i < _patientList.length; i++) {
+            if (_patientList[i].sickBedCode === _sickBedCode) {
+                return _patientList[i];
+            }
+        }
+        return null;
+    }
+
     let template = dashboardSickRoomList;
     let templateSickRoom = ``;
     let patients = await getPatientList();
+    let sickRoomAndSickBed = {};
+    sickRoomList.forEach((room) => {
+        sickRoomAndSickBed[`${room.sickRoomCode}`] = [];
+    });
+    sickBedList.forEach((bed) => {
+        sickRoomAndSickBed[`${bed.sickRoomCode}`].push(bed);
+    });
+
+    let sickBedsTmpls = {};
+    sickRoomList.forEach((sickRoom) => {
+        let sickBedsTmpl = `<div class="patient_info ${sickRoom.sickRoomCode}">`;
+        sickRoomAndSickBed[`${sickRoom.sickRoomCode}`].forEach((sickBed) => {
+            if (sickBed.measurementCode) {
+                const { patientCode, name, age, gender } =
+                    findPatientBySickBedCode(sickBed.sickBedCode, patients);
+                sickBedsTmpl += `<div class="input_wrap">
+                    <input
+                        type="checkbox"
+                        name="sickBed_no"
+                        class="green_custom"
+                        id="${sickBed.sickBedCode}"
+                    />
+                    <label for="${sickBed.sickBedCode}"></label>
+                    <label for="${sickBed.sickBedCode}">
+                        <span>${sickBed.sickBed},${name}(${age}.${
+                    gender === 1 ? '남' : '여'
+                }.${patientCode})</span></label>
+                </div>`;
+            } else {
+                sickBedsTmpl += `<div class="input_wrap">
+                    <input
+                        type="checkbox"
+                        name="sickBed_no"
+                        class="green_custom"
+                        id="${sickBed.sickBedCode}"
+                    />
+                    <label for="${sickBed.sickBedCode}"></label>
+                    <label for="${sickBed.sickBedCode}">
+                        <span>${sickBed.sickBed}</span></label>
+                </div>`;
+            }
+        });
+        sickBedsTmpl += '</div>';
+        sickBedsTmpls[`${sickRoom.sickRoomCode}`] = sickBedsTmpl;
+    });
     sickRoomList.forEach((sickRoom) => {
         templateSickRoom += `<div class='ward_block ${sickRoom.wardCode}'>
             <div class="ward_count ${sickRoom.sickRoomCode}">
@@ -15,20 +69,13 @@ export const parseSickBedListLeft = async (sickRoomList, sickBedList) => {
                         name="room_no"
                     />
                     <label for="${sickRoom.sickRoomCode}"></label>
-                    <label for="${sickRoom.sickRoomCode}">${sickRoom.sickRoom}</label>
+                    <label for="${sickRoom.sickRoomCode}">${
+            sickRoom.sickRoom
+        }</label>
                 </div>
             </div>
-            {{sickBedTmpl}}
+            ${sickBedsTmpls[sickRoom.sickRoomCode]}
         </div>`;
-
-        let sickBedsBySickRoom = sickBedList.filter(
-            (bed) => bed.sickRoomCode === sickRoom.sickRoomCode
-        );
-        console.log(sickBedsBySickRoom);
-        let patientsBySickRoom = patients.filter(
-            (patient) => patient.sickRoomCode === sickRoom.sickRoomCode
-        );
-        console.log(patientsBySickRoom);
     });
 
     return template.replace('{{tmpl}}', templateSickRoom);
